9e7fd5bb277781a5a4b6bb3abf26e59e7f413036
corerage <nwaigboebube@gmail.com>
2022-10-10 11:52:46 +0100

    debug deployment

---
+++ q4.py
@@ -0,0 +1,10 @@
+from nturl2path import url2pathname
+import requests
+
+url = "http://localhost:9696/q4_predict"
+
+
+client = {"reports": 0, "share": 0.245, "expenditure": 3.438, "owner": "yes"}
+response = requests.post(url, json=client).json()
+
+print(response)
